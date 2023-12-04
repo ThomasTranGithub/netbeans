13b693d29442eee5ff1b8150ef814bb4e7fbe33d
Thomas Tran, MBA <80990607+ThomasTranGithub@users.noreply.github.com>
2023-12-03 21:48:30 -0500

    test

---
+++ src/asset/Asset.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package asset;
+
+/**
+ *
+ * @author thomastran
+ */
+public class Asset {
+
+   //methods
+    public double getVolatility(){
+        return 0;
+    }
+    public double getPrice(){
+        return 0;
+    }
+    public boolean setPrice(double newPrice){
+        return false;
+    }
+    public boolean setVolatility(double newVolatility){
+        return false;
+    }
+    //attributes
+    private double Volatility;
+    private double Price;     
+        // TODO code application logic here
+    }
+    
